d5df16bd20ffab176a3409a1838168a6c44548a8
Littlewolfwork <36741698+Littlewolfwork@users.noreply.github.com>
2019-05-10 20:47:54 +0500

    version 1.01

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var btn = document.querySelector('#btn');
 btn.addEventListener("click", function(){
+    // TODO: добавить проверку введенных значений
     const firstNumber = parseFloat(document.querySelector("#firstNumber").value);
     const secondNumber = parseFloat(document.querySelector("#secondNumber").value);
     window.alert(firstNumber+secondNumber);
-
 });
 
