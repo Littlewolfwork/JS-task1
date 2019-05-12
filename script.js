7972d672d282ece2a14e63e4051c1fed5944844c
Littlewolfwork <36741698+Littlewolfwork@users.noreply.github.com>
2019-05-12 17:10:41 +0500

    Version 1.1
    Добавлена проверка введенных значений

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 var btn = document.querySelector('#btn');
 btn.addEventListener("click", function(){
-    // TODO: добавить проверку введенных значений
     const firstNumber = parseFloat(document.querySelector("#firstNumber").value);
     const secondNumber = parseFloat(document.querySelector("#secondNumber").value);
+    if (!(isFinite(firstNumber) && isFinite(secondNumber))){
+        window.alert("Введено некорректное значение!");
+        return;
+    }
     let result = firstNumber+secondNumber;
     window.alert( +result.toFixed(15));
 });
