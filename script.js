98e83ffe9624ec5efc5f2548cfec50e5aad32723
Littlewolfwork <36741698+Littlewolfwork@users.noreply.github.com>
2019-05-12 16:25:52 +0500

    Version 1.02
    исправлено, что 0,1+0,2 не равно 0,3

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ btn.addEventListener("click", function(){
     // TODO: добавить проверку введенных значений
     const firstNumber = parseFloat(document.querySelector("#firstNumber").value);
     const secondNumber = parseFloat(document.querySelector("#secondNumber").value);
-    window.alert(firstNumber+secondNumber);
+    let result = firstNumber+secondNumber;
+    window.alert( +result.toFixed(15));
 });
 
